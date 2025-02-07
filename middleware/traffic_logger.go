e0a73b937a294066b5d8f1a5fb639a4a42b006f2
Yiheng-Zhou <Yiheng.Zhou@sanofi.com>
2025-02-07 15:43:31 +0800

    feat: max request and response length for logging

--- middleware/traffic_logger.go
+++ middleware/traffic_logger.go
@@ -11,14 +11,21 @@ import (
 	"github.com/cloudwego/hertz/pkg/app"
 )
 
-func HertzTrafficLogger() app.HandlerFunc {
+func HertzTrafficLogger(maxRequestLen, maxResponseLen uint) app.HandlerFunc {
 	return func(ctx context.Context, c *app.RequestContext) {
 		ctx = logid.ContextWithLogId(ctx, idgen.New[string]())
 		reqStr := ghttp.DumpHertzRequest(&c.Request)
 		startAt := time.Now()
+		if len(reqStr) > int(maxRequestLen) {
+			reqStr = reqStr[:maxRequestLen]
+		}
 		logs.Infof(ctx, "[TrafficLogger.Request] %s, body:\n%s", startAt.Format(time.RFC3339), reqStr)
 		c.Next(ctx)
 		resp := c.GetResponse()
-		logs.Infof(ctx, "[TrafficLogger.Response] status code: %d, cost: %d ms, body:\n%s", resp.StatusCode(), time.Since(startAt).Milliseconds(), resp.Body())
+		respStr := resp.Body()
+		if len(respStr) > int(maxResponseLen) {
+			respStr = respStr[:maxResponseLen]
+		}
+		logs.Infof(ctx, "[TrafficLogger.Response] status code: %d, cost: %d ms, body:\n%s", resp.StatusCode(), time.Since(startAt).Milliseconds(), respStr)
 	}
 }
